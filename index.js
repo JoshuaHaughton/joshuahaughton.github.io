9beee6c1870dcfd05cd4ca921dc723c6202d6f50
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-01-29 10:48:17 -0500

    Add floating objects in background to improve user experience

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+const moveBackground = (event) => {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+};
 
 const toggleContrast = () => {
   contrastToggle = !contrastToggle;
